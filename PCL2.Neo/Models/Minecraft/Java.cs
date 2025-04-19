80a068eaf6150c2f7be24ded5b8772624fcccfa2
Hill23333 <104297546+Hill23333@users.noreply.github.com>
2025-04-19 21:28:23 +0800

    Revert "重构部分代码"

---
+++ PCL2.Neo/Models/Minecraft/Java.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PCL2.Neo.Models.Minecraft.JavaSearcher;
+
+namespace PCL2.Neo.Models.Minecraft
+{
+    /// <summary>
+    /// 测试
+    /// </summary>
+    public class Java
+    {
+        public static async Task<List<JavaEntity>> SearchJava()
+        {
+            var javaList = new List<JavaEntity>();
+
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.Win32NT:
+                    javaList.AddRange(await PCL2.Neo.Models.Minecraft.JavaSearcher.Windows.SearchJava());
+                    break;
+                case PlatformID.Unix:
+                    javaList.AddRange(Unix.SerachJava());
+                    break;
+                default:
+                    throw new PlatformNotSupportedException();
+            }
+
+            return javaList;
+        }
+    }
+}
