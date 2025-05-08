767cde3d2dd55aca5794b8445ecb71590e088c75
AMagicPear <AMagicPear@outlook.com>
2025-05-08 16:23:11 +0800

    feat&fix: 更新Java列表的UI，使用更具描述性的字段并优化数据绑定，修复了诡异现象

--- PCL2.Neo/ViewModels/Setup/SetupLaunchViewModel.cs
+++ PCL2.Neo/ViewModels/Setup/SetupLaunchViewModel.cs
@@ -1,29 +1,43 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using PCL2.Neo.Models.Minecraft.Java;
-using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 
 namespace PCL2.Neo.ViewModels.Setup;
+
+public record JavaUiInfo(JavaRuntime Runtime)
+{
+    public string Identifier =>
+        $"{(Runtime.IsJre ? "JRE" : "JDK")} {Runtime.SlugVersion} ({Runtime.Version}) {Runtime.Architecture}";
+
+    public string Path => Runtime.DirectoryPath;
+}
 
 [SubViewModelOf(typeof(SetupViewModel))]
 public partial class SetupLaunchViewModel : ViewModelBase
 {
     private readonly IJavaManager _javaManager;
-    [ObservableProperty] private List<JavaRuntime> _javaList = [];
+    [ObservableProperty] private ObservableCollection<JavaUiInfo> _javaInfoList = [];
+
+    private void DoUiRefresh()
+    {
+        if (JavaInfoList.Count != 0) JavaInfoList.Clear();
+        foreach (JavaRuntime runtime in _javaManager.JavaList)
+            JavaInfoList.Add(new JavaUiInfo(runtime));
+    }
 
     public SetupLaunchViewModel(IJavaManager javaManager)
     {
         _javaManager = javaManager;
-        JavaList = _javaManager.JavaList;
+        DoUiRefresh();
     }
 
     [RelayCommand]
     private async Task RefreshJava()
     {
+        JavaInfoList.Clear();
         await _javaManager.Refresh();
-        JavaList = _javaManager.JavaList;
-        Console.WriteLine($"UI显示列表有{JavaList.Count}个Java");
+        DoUiRefresh();
     }
 }
